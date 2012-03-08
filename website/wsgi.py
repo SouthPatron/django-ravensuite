8bb292bcb92f4489c363d70a3980be20a337643c
Durand John Miller <durand.miller@smksoftware.com>
2012-03-08 14:55:35 +0200

    Make site locations less hard-coded

--- website/wsgi.py
+++ website/wsgi.py
@@ -2,9 +2,11 @@ import os,sys
 
 WEBSITE_BASE = os.path.dirname( os.path.dirname( __file__ ) )
 
+MODULE_NAME = 'website'
+
 sys.path.append( WEBSITE_BASE )
-sys.path.append( WEBSITE_BASE + '/' + __name__ )
-os.environ.setdefault( "DJANGO_SETTINGS_MODULE", __name__ + '.settings' )
+sys.path.append( WEBSITE_BASE + '/' + MODULE_NAME )
+os.environ.setdefault( "DJANGO_SETTINGS_MODULE", MODULE_NAME + '.settings' )
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
