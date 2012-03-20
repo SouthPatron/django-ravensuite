abcba012b524febdf4f7cfabe476b88759225451
Durand John Miller <durand.miller@smksoftware.com>
2012-03-20 11:59:26 +0200

    Kill the local daemon process

--- website/common/views/helps.py
+++ website/common/views/helps.py
@@ -2,16 +2,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import redirect
 
-
 import signal, os
 
 
 def restart( request ):
-	try:
-		if os.environ['mod_wsgi.process_group'] != '':
-			os.kill(os.getpid(), signal.SIGINT)
-	except KeyError, ke:
-		pass
+	os.kill(os.getpid(), signal.SIGINT)
 	return redirect( '/' )
 
 
