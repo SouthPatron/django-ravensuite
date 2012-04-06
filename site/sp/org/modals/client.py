288b312002a9b52f913db3077de2325591a404df
Durand John Miller <durand.miller@smksoftware.com>
2012-04-06 16:30:54 +0200

    Get rid of delete for now.

--- site/sp/org/modals/client.py
+++ site/sp/org/modals/client.py
@@ -93,22 +93,4 @@ class AccountTransactionAllocate( ModalLogic ):
 
 
 
-class DeleteClient( ModalLogic ):
-
-	def get_extra( self, request, dmap, obj, *args, **kwargs ):
-		return None
-
-	def get_object( self, request, dmap, *args, **kwargs ):
-		return Client.objects.get( refnum = dmap[ 'cid' ], organization__refnum = dmap[ 'oid' ] )
-
-	def get( self, request, dmap, obj, extra, fmt, *args, **kwargs ):
-		self.easy.confirm()
-		self.additional = {
-			'message' : _( 'Are you sure you want to delete this client? This can not be undone' )
-		}
-
-	def perform( self, request, dmap, obj, extra, fmt, *args, **kwargs ):
-		messages.success( request, _( 'Client was deleted' ) )
-		self.easy.notice();
-
 
