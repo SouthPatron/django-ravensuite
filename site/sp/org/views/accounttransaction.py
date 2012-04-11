80d7bc88f44b1045ba1478a4c843599285982f2e
Durand John Miller <durand.miller@smksoftware.com>
2012-04-11 23:13:06 +0200

    Integrated highcharts with the single account page

--- site/sp/org/views/accounttransaction.py
+++ site/sp/org/views/accounttransaction.py
@@ -9,6 +9,8 @@ from common.views.listview import ListView
 from common.models import *
 from common.utils.dbgdatetime import datetime
 
+#from email.utils import parsedate_tz
+#print parsedate_tz('Fri, 15 May 2009 17:58:28 +0700')
 
 class AccountTransactionList( ListView ):
 	template_name = 'pages/org/client/account/transaction/index'
@@ -17,7 +19,20 @@ class AccountTransactionList( ListView ):
 		return Account.objects.get( client__organization__refnum = self.url_kwargs.oid, client__refnum = self.url_kwargs.cid )
 
 	def get_object_list( self, request, *args, **kwargs ):
-		obj_list = AccountTransaction.objects.filter( account__client__refnum = self.url_kwargs.cid, account__client__organization__refnum = self.url_kwargs.oid )
+
+		qd = self._get_query_data( request )
+
+		obj_list = AccountTransaction.objects.filter( account__client__refnum = self.url_kwargs.cid, account__client__organization__refnum = self.url_kwargs.oid ).order_by( '-event_time' )
+
+		if qd.get( 'days', None ) is not None:
+			now = datetime.date.today()
+			now = now - datetime.timedelta( days = int(qd[ 'days' ]) )
+
+			obj_list = obj_list.filter( event_time__gte = now )
+			
+
+
+
 		return obj_list
 
 
