fbbc3d005b7b53f9dd1eecf720ee6cd6a9c727ef
Durand John Miller <durand.miller@smksoftware.com>
2012-04-28 15:00:47 +0200

    Correct an action

--- static/static/custom/ral/ral.js
+++ static/static/custom/ral/ral.js
@@ -3,14 +3,16 @@ ral.html.custom = {}
 
 ral.html.custom.attach_fetch = function( id, modal_name, pdata ) {
 	jQuery( id ).click( function() {
-		var url = '/modals/' + modal_name;
+		var tokens = modal_name.split('.');
+		var url = '/v/' + tokens[0] + '/modals/' + modal_name;
 		ral.html.fetch( url, pdata );
 	});
 }
 
 ral.html.custom.attach_post = function( id, modal_name, pdata ) {
 	jQuery( id ).click( function() {
-		var url = '/modals/' + modal_name;
+		var tokens = modal_name.split('.');
+		var url = '/v/' + tokens[0] + '/modals/' + modal_name;
 		ral.html.post( url, pdata );
 	});
 }
