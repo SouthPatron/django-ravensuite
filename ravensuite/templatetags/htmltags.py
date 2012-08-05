cbe75e55649ed4e803362c0d8a2fbc667a49558c
Durand John Miller <durand.miller@smksoftware.com>
2012-08-05 14:35:36 +0200

    html tags experiment

---
+++ ravensuite/templatetags/htmltags.py
@@ -0,0 +1,16 @@
+from django import template
+from django.conf import settings
+
+
+register = template.Library()
+
+@register.simple_tag
+def raven_javascript( request, base ):
+
+	if settings.DEBUG is True:
+		return '<!-- DEBUG IS TRUE ' + base + ' -->'
+	else:
+		return '<!-- DEBUG IS false ' + base + ' -->'
+
+
+
