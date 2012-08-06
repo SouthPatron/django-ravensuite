9de42ec3e5bd84036bc730084a7cfb2c86435402
Durand John Miller <durand.miller@smksoftware.com>
2012-08-06 15:57:36 +0200

    Bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 		name = 'django-ravensuite',
-		version = '0.2.5',
+		version = '0.2.6',
 		author = 'SMK Software',
 		author_email = 'support@smksoftware.com',
 		packages = [ 'ravensuite', ],
