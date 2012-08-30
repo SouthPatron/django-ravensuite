01e8d4eb7a3f7c57ab868785da9da311672d8361
Durand John Miller <durand.miller@smksoftware.com>
2012-08-30 12:17:02 +0200

    Include packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 		name = 'ravensuite',
-		version = '0.2.12',
+		version = '0.2.13',
 		author = 'SMK Software',
 		author_email = 'support@smksoftware.com',
 		packages = [ 'ravensuite', ],
