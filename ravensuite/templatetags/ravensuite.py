93b4a93cc804a1a9901c57fa41bacd50c06fff47
Durand John Miller <durand.miller@smksoftware.com>
2013-01-10 13:56:10 +0200

    Captureas included into raven

--- ravensuite/templatetags/ravensuite.py
+++ ravensuite/templatetags/ravensuite.py
@@ -37,3 +37,23 @@ def raven_css():
 	return rc
 
 
+@register.tag(name='captureas')
+def captureas(parser, token):
+	try:
+		tag_name, args = token.contents.split(None, 1)
+	except ValueError:
+		raise template.TemplateSyntaxError("'captureas' node requires a variable name.")
+	nodelist = parser.parse(('endcaptureas',))
+	parser.delete_first_token()
+	return CaptureasNode(nodelist, args)
+
+class CaptureasNode(template.Node):
+	def __init__(self, nodelist, varname):
+		self.nodelist = nodelist
+		self.varname = varname
+
+	def render(self, context):
+		output = self.nodelist.render(context)
+		context[self.varname] = output
+		return ''
+
